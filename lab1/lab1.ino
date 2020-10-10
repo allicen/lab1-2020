ee657a699fb923a55f2839b651960e2ffa511885
Elena Rubleva <ellena.hella@yandex.ru>
2020-10-10 17:36:38 +0500

    add files

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include <MD_TCS230.h>
+#include "pitches.h"
+#include "button.h"
+#include "buzzer.h"
 
 #define  S0_OUT  2
 #define  S1_OUT  3
@@ -10,7 +13,16 @@
 #define G_OUT 7
 #define B_OUT 8
 
+// назначить кнопки пинам
+Button buttonR(S0_OUT);
+Button buttonG(S1_OUT);
+Button buttonB(S2_OUT);
+
+
 MD_TCS230 colorSensor(S2_OUT, S3_OUT, S0_OUT, S1_OUT);
+
+//Компоненты: макетная плата + 3 кнопки + RGB светодиод.
+//Описание: через Serial приходят компоненты цвета для светодиода. Каждая кнопка включает/выключает одну из компонент цвета.
 
 void setup()
 {
@@ -18,14 +30,14 @@ void setup()
     Serial.println("Started!");
 
     sensorData whiteCalibration;
-    whiteCalibration.value[TCS230_RGB_R] = 0;
-    whiteCalibration.value[TCS230_RGB_G] = 0;
-    whiteCalibration.value[TCS230_RGB_B] = 0;
+    whiteCalibration.value[TCS230_RGB_R] = 83440;
+    whiteCalibration.value[TCS230_RGB_G] = 78370;
+    whiteCalibration.value[TCS230_RGB_B] = 271630;
 
     sensorData blackCalibration;
-    blackCalibration.value[TCS230_RGB_R] = 0;
-    blackCalibration.value[TCS230_RGB_G] = 0;
-    blackCalibration.value[TCS230_RGB_B] = 0;
+    blackCalibration.value[TCS230_RGB_R] = 10820;
+    blackCalibration.value[TCS230_RGB_G] = 9750;
+    blackCalibration.value[TCS230_RGB_B] = 35910;
 
     colorSensor.begin();
     colorSensor.setDarkCal(&blackCalibration);
@@ -46,6 +58,16 @@ void loop()
     colorSensor.getRGB(&rgb);
     print_rgb(rgb);
     set_rgb_led(rgb);
+
+    if (buttonR.wasPressed()) {
+      
+    }
+    if (buttonG.wasPressed()) {
+      
+    }
+    if (buttonB.wasPressed()) {
+      
+    }
 }
 
 void print_rgb(colorData rgb)
